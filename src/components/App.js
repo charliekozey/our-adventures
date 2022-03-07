133825176767d75ddcd5b9d54c0f50ad560ab211
Eric Van <ggclarence@yahoo.com>
2022-03-07 12:44:11 -0600

    Set up Json Server to link with npm

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./Header";
 import NavBar from "./NavBar";
 import TripCard from "./TripCard";
@@ -6,6 +6,8 @@ import NewForm from "./NewForm";
 import '../App.css';
 
 function App() {
+  
+
   return (
     <>
       <Header />
