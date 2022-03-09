f7cb26e39d19d0999a442b93de34b0eae3136bbb
Paola Moreno Reyes <nailamoreno99@gmail.com>
2022-03-09 12:09:23 -0600

    granit helped w uneven bars

--- src/components/NewForm.js
+++ src/components/NewForm.js
@@ -1,31 +1,33 @@
 import React, { useState } from "react";
 
-function NewForm({addAdventure}) {
+function NewForm({ addAdventure }) {
 
-const [newBlog,setnewBlog] = useState([])
+    const [newBlog, setnewBlog] = useState([])
 
-    function handleSumbit(event){
+    function handleSumbit(event) {
         event.preventDefault()
         addAdventure(newBlog)
-     }
+    }
 
-     function onChange(event){
-         setnewBlog({...newBlog,[event.target.name]:event.target.value})
-     }
+    function onChange(event) {
+        setnewBlog({ ...newBlog, [event.target.name]: event.target.value })
+    }
 
 
-    return (      
-        <div className="newBlog"> 
-        <h2 className="newBlogTitle">Add your adventure!</h2>
-        <form id="form" onSubmit={handleSumbit}>
-            <fieldset className="formFieldSet">
-            <input className="formName" type="text" name="name" placeholder="Your name" onChange={onChange}/>
-            <input className="formLocation" type="text" name="location" placeholder="Where did you go?" onChange={onChange}/>
-            <input className="formImage" type= "text" name="image" placeholder="Image URL" onChange={onChange}/>
-            <input className="formDescription" typeof="text" name="description" placeholder="Tell us about it!" onChange={onChange}/>
-            <button className= "submitButtonDisplay" type="submit">Add adventure</button>
-            </fieldset>
-        </form>
+    return (
+        <div className="newBlog">
+            <h2 className="newBlogTitle">Add your adventure!</h2>
+            <form id="form" onSubmit={handleSumbit}>
+                <fieldset className="formFieldSet">
+                    <ul>
+                        <input className="formName" type="text" name="name" placeholder="Your name" onChange={onChange} />
+                        <input className="formLocation" type="text" name="location" placeholder="Where did you go?" onChange={onChange} />
+                        <input className="formImage" type="text" name="image" placeholder="Image URL" onChange={onChange} />
+                        <input className="formDescription" typeof="text" name="description" placeholder="Tell us about it!" onChange={onChange} />
+                        <button className="submitButtonDisplay" type="submit">Add adventure</button>
+                    </ul>
+                </fieldset>
+            </form>
         </div>
     )
 }
