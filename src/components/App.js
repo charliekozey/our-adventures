c76dcb104c046f55dc0c75719b17d069d8f2fc2e
Granit <gzymeri55@gmail.com>
2022-03-10 23:19:56 +0100

    updated

--- src/components/App.js
+++ src/components/App.js
@@ -8,33 +8,14 @@ import '../App.css';
 
 
 function App() {
-  const [tripsArray, setTripsArray] = useState([])
-  const tripsURL = ("http://localhost:3000/trips")
-
-  useEffect(() => {
-  fetch(tripsURL)
-    .then(resp => resp.json())
-    .then(data => setTripsArray(data))
-  },[])
-
-
-  // Form Submit
-function addAdventure(newBlog){
-  console.log(newBlog)
-  // setTripsArray({...tripsArray, newBlog})
-  // Uncomment after NAVBAR is fixed
-  console.log(tripsArray)
-}
-  // Form Submit
-
 
 
   return (
     <>
       <Header />
-      <NavBar tripsArray={tripsArray} />
+      <NavBar />
       <TripCard />
-      <NewForm addAdventure={addAdventure}/>
+      <NewForm />
     </>
   );
 }
