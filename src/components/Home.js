0418126c7a6424f3131dc6a834b93d08abac46b7
charliekozey <charkoz@mailbox.org>
2022-03-10 21:40:46 -0500

    home page center and same color

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,17 +4,17 @@ import { NavLink } from "react-router-dom";
 function Home() {
 
     return (
-        <>
-            <h1>
+        <div class="trip-window">
+            <h1 id="welcome">
                 Welcome to Our Adventures!
             </h1>
-            <p>Check out our adventures by clicking the links above, or submit your own adventure to the collection.</p>
+            <h4 id="about">Check out our adventures by clicking the links above, or submit your own adventure to the collection.</h4>
             <button id="submit-button">
                 <NavLink to="/newForm">
                     Add your adventure
                 </NavLink>
             </button>
-        </>
+        </div>
     )
 }
 
